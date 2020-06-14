a15f04cf2cb830f31b5b0a5d5cff3f383f3b2865
debojyoti <debojyoti.js@gmail.com>
2020-06-14 20:08:30 +0530

    Working on initial pages

--- src/containers/force-credentails/force-credentails.jsx
+++ src/containers/force-credentails/force-credentails.jsx
@@ -1,7 +1,61 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./force-credentails.scss";
 
 const ForceCredentials = (props) => {
+  const [currentView, updateCurrentView] = useState({
+    modeName: "decisonMaking",
+  });
+
+  const _renderView = () => {
+    switch (currentView.modeName) {
+      case "deviceNameInput": {
+        return (
+          <div className="row">
+            <div className="col-md-12" id="getStartedWrapper">
+              <h5>Hey, name your device to get started</h5>
+              <div className="custom-input-group">
+                <p>Device name</p>
+                <input
+                  type="text"
+                  className="custom-input"
+                  placeholder="Eg: John-Dell-Laptop"
+                />
+              </div>
+              <div className="buttonWrapper">
+                <button className="custom-button">Get Started &nbsp; <i className="fas fa-arrow-right"></i></button>
+                {/* <div className="loader"></div> */}
+              </div>
+            </div>
+          </div>
+        );
+			}
+			case "decisonMaking": {
+        return (
+          <div className="row">
+            <div className="col-md-12" id="getStartedWrapper">
+              <h5>Hey, name your device to get started</h5>
+              <div className="custom-input-group">
+                <p>Device name</p>
+                <input
+                  type="text"
+                  className="custom-input"
+                  placeholder="Eg: John-Dell-Laptop"
+                />
+              </div>
+              <div className="buttonWrapper">
+                <button className="custom-button">Get Started &nbsp; <i className="fas fa-arrow-right"></i></button>
+                {/* <div className="loader"></div> */}
+              </div>
+            </div>
+          </div>
+        );
+      }
+      default: {
+        return <div />;
+      }
+    }
+  };
+
   return (
     <div id="forceCredentialsWrapper">
       <div className="container" id="forceCredentialsInnerWrapper">
@@ -20,15 +74,7 @@ const ForceCredentials = (props) => {
               <i class="fas fa-code"></i> codes across devices
             </p>
           </div>
-          <div className="col-md-7">
-						<div className="row">
-							<div className="col-md-12" id="getStartedWrapper">
-								<h5>Get started by creating a new room</h5>
-								<p>NickName</p>
-								<input type="text" className="custom-input" placeholder="Nickname"/>
-							</div>
-						</div>
-					</div>
+          <div className="col-md-7">{_renderView()}</div>
         </div>
       </div>
     </div>
